862a11d1af395cdc235f7a5f67d3665991e20a32
Bartlomiej <trafficap@gmail.com>
2023-07-28 17:58:42 +0200

    main element with form

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,22 @@
-import Image from "next/image";
+'use client';
+import DropdownSection from './components/DropdownSection';
+import GenerateButton from './components/GenerateButton';
+import TextField from './components/TextField';
 
 export default function Home() {
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+  }
+
   return (
-    <main className="">
-      <p>yoł</p>
+    <main className="flex flex-col items-center w-full h-screen bg-zinc-900 pt-8">
+      <form className="flex flex-col items-center" onSubmit={handleSubmit}>
+        <TextField label="Page content" />
+        <DropdownSection label="Prompt" open={false}>
+          <TextField />
+        </DropdownSection>
+        <GenerateButton />
+      </form>
     </main>
   );
 }
