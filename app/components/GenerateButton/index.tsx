b1ae0d2bcc79d18a04eab2f6589cfc59056ddc68
Bartlomiej <trafficap@gmail.com>
2023-07-28 17:53:47 +0200

    add GenerateButton as submit component

---
+++ app/components/GenerateButton/index.tsx
@@ -0,0 +1,12 @@
+function GenerateButton() {
+  return (
+    <button
+      type="submit"
+      className="bg-zinc-800 border border-zinc-500 hover:bg-zinc-700 text-zinc-300 font-bold h-12 w-80"
+    >
+      Generate
+    </button>
+  );
+}
+
+export default GenerateButton;
