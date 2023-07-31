dcd35c7f92895e81d34def17a303eb0ef2955897
Bartlomiej <trafficap@gmail.com>
2023-07-31 10:36:46 +0200

    changed main page components to react hook form

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,96 @@
 'use client';
+import { useEffect, useState } from 'react';
 import DropdownSection from './components/DropdownSection';
-import GenerateButton from './components/GenerateButton';
-import TextField from './components/TextField';
+import { useForm } from 'react-hook-form';
+import { createPrompt } from './utils/aiUtils';
+
+type ProjectFormData = {
+  nameOfProject: string;
+  websiteContent: string;
+  namesOfParticipants: string;
+  lengthOfTheReview: string;
+  customPrompt?: string;
+};
 
 export default function Home() {
-  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
-    event.preventDefault();
-  }
+  const [prompt, setPrompt] = useState('');
+  const [completion, setCompletion] = useState('');
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<ProjectFormData>();
+
+  const formFields = watch();
+
+  useEffect(() => {
+    const prompt = createPrompt(formFields);
+    setPrompt(prompt);
+  }, [formFields]);
+
+  const onSubmit = (data: ProjectFormData) => {
+    console.log(data);
+  };
 
   return (
     <main className="flex flex-col items-center w-full h-screen bg-zinc-900 pt-8">
-      <form className="flex flex-col items-center" onSubmit={handleSubmit}>
-        <TextField label="Page content" />
+      <form
+        className="flex flex-col items-center"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <label htmlFor="nameOfProject">
+          <p className="text-zinc-300 pb-3">Project name</p>
+          <input
+            {...register('nameOfProject', { required: true })}
+            className="w-80 bg-zinc-800 p-3 text-zinc-300 resize-none "
+          />
+        </label>
+        <label htmlFor="namesOfParticipants">
+          <p className="text-zinc-300 pb-3">
+            Names of participants separeted by " , "
+          </p>
+          <input
+            {...register('namesOfParticipants', { required: true })}
+            placeholder="Viki, James"
+            className="w-80 bg-zinc-800 p-3 text-zinc-300 resize-none "
+          />
+        </label>
+        <label htmlFor="lengthOfTheReview">
+          <p className="text-zinc-300 pb-3">Length of the review in minutes.</p>
+          <input
+            {...register('lengthOfTheReview', { required: true })}
+            placeholder="10"
+            className="w-80 bg-zinc-800 p-3 text-zinc-300 resize-none "
+          />
+        </label>
+
+        <label htmlFor="websiteContent">
+          <p className="text-zinc-300 pb-3">Page content</p>
+          <textarea
+            {...register('websiteContent', { required: true })}
+            className="w-80 bg-zinc-800 p-3 text-zinc-300 resize-none "
+            rows={10}
+          />
+        </label>
+
         <DropdownSection label="Prompt" open={false}>
-          <TextField />
+          <textarea
+            value={prompt}
+            onChange={(e) => setPrompt(e.target.value)}
+            className="w-80 bg-zinc-800 p-3 text-zinc-300 resize-none "
+            rows={10}
+          />
         </DropdownSection>
-        <GenerateButton />
+
+        <button
+          type="submit"
+          className="bg-zinc-800 border border-zinc-500 hover:bg-zinc-700 text-zinc-300 font-bold h-12 w-80"
+        >
+          Generate
+        </button>
       </form>
+      <article>{completion}</article>
     </main>
   );
 }
